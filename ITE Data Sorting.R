de304288338dc507338e13d86ab30b8e4ada5b1d
ITE Management <ITE Management@ad.itemgmt.com>
2019-12-06 09:33:00 -0600

    Updated w/ Dollar Updates

--- ITE Data Sorting.R
+++ ITE Data Sorting.R
@@ -1,5 +1,8 @@
 myData <- read.csv("2019 All Data.csv")
 
-myData <- transform(myData,RepairFacilityOwnershipType=revalue(RepairFacilityOwnershipType,c("Rail Road"="Railroad")))
+data <- transform(data,RepairFacilityOwnershipType=revalue(RepairFacilityOwnershipType,c("Rail Road"="Railroad")))
 
-MyData <- filter(myData, myData$RepairFacilityOwnershipType)
+MyData <- filter(data, ChargeDollars>0)
+
+data<- data %>%
+   mutate(myID = group_indices(., CarID, RepairDate, RepairFacilityName))
